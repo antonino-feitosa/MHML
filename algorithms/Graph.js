796fc5d1fff0fb9ce5add0137e5dff56f940bd49
antonino-feitosa <antonino_feitosa@yahoo.com.br>
2022-12-02 17:31:38 -0300

    Add Graph class with tests and code documentation

--- algorithms/Graph.js
+++ algorithms/Graph.js
@@ -1,13 +1,23 @@
 
+/** Class representing edges connecting nodes of a graph.*/
 class Edge {
 
+	/** Creates a edge connecting a `source` node with a destination node `dest` with the cost `cost`.
+		@param {*} source - The source node.
+		@param {*} dest - The destination node.
+		@param {*} cost - The cost of the connection.
+	 */
 	constructor(source, dest, cost) {
 		this.source = source;
 		this.dest = dest;
 		this.cost = cost;
 	}
 
-	contains(node){
+	/** Checks if a `node` is connected by this edge.
+		@param {*} node - The node being checked.
+		@returns true if the node is connected and false otherwise.
+	 */
+	contains(node) {
 		return this.source === node || this.dest === node;
 	}
 
@@ -17,50 +27,67 @@ class Edge {
 		} else if (node === this.dest) {
 			return this.source;
 		} else {
-			throw node.value + ' is not a neighbor';
+			throw new Error(node.value + ' is not a neighbor');
 		}
 	}
 }
 
+/**Class representing nodes of a graph.*/
 class Node {
 
+	/**	Creates a node with specified `value` and `graph`.
+		It must be used only by the graph.
+		@param {*} value - The value of the node also used as a key in the graph.
+		@param {*} graph - The graph of the node.
+	 */
 	constructor(value, graph) {
 		this.value = value;
 		this.edges = [];
 		this.graph = graph;
 	}
 
+	/** Connects this node with another `node`.
+		@param {Node} node - The node being connected.
+		@param {Number} cost - The cost of the connection associated with the edge.
+		@param {Boolean} directed - Indicates if the connection is directed or undirected.
+		@returns {Edge} The edge representing the connection.
+		@throws {Error} There must not be a connection between the nodes.
+	 */
 	connect(node, cost = 0, directed = true) {
-		if(this.edges.find(e => e.contains(node)))
-			throw node.value + ' is a neighbor';
+		if (this.edges.find(e => e.contains(node)))
+			throw new Error(node.value + ' is a neighbor');
 
 		let edge = new Edge(this, node, cost);
 		this.edges.push(edge);
-		if(!directed){
+		if (!directed) {
 			node.edges.push(edge);
 		}
 		this.graph.edges.push(edge);
 		return edge;
 	}
 
-	hasPathTo(node){
+	/** Checks if there is a path to `node`
+		@param {*} node - The node being checked.
+		@returns true if there is a path and false otherwise
+	 */
+	hasPathTo(node) {
 		return this.edges.some(e => e.contains(node));
 	}
 
-	/**	Get the cost to a adjacent node.
-	  	@param {Any} node - A adjacent node.
-	 	@returns {Number} The associated cost
+	/**	Get the cost to a adjacent `node`.
+		@param {*} node - A adjacent node.
+		@returns {Number} The associated cost
 		@throws {Error} The node must be adjacent.
 	 */
-	costTo(node){
+	costTo(node) {
 		let edge = this.edges.find(e => e.contains(node));
-		if(!edge)
+		if (!edge)
 			throw new Error(node.value + ' is not a neighbor');
 		return edge.cost;
 	}
 
 	/**	Get the adjacents nodes.
-	 	@returns {Array} A array with the adjacent nodes. It will be empty if none.
+		@returns {Array} A array with the adjacent nodes. It will be empty if none.
 	 */
 	adjacents() {
 		let nodes = this.edges.map(e => e.opposite(this));
@@ -92,17 +119,17 @@ class Graph {
 	}
 
 	/**	@property {Number} - The number of nodes. */
-	get numNodes(){
+	get numNodes() {
 		return this.nodes.length;
 	}
 
 	/**	@property {Number} - The number of edges. */
-	get numEdges(){
+	get numEdges() {
 		return this.edges.length;
 	}
 
-	/**	Insert a new node with data value
-		@param {Any} value - The value of the node.
+	/**	Insert a new node with data `value`
+		@param {*} value - The value of the node.
 		@return {Node}		 The created node.
 	*/
 	add(value) {
@@ -112,9 +139,9 @@ class Graph {
 		return node;
 	}
 
-	/**	Get the node associated with the value
-		@param {Any} value - The value of a node present in the graph.
-		@return {Node}		 The correspondent node if the value is present, or null otherwise.
+	/**	Get the node associated with the `value`
+		@param {*} value - The value of a node present in the graph.
+		@return {Node}		 The correspondent node if the `value` is present, or null otherwise.
 	*/
 	get(value) {
 		return this.valueToNode.get(value);
--- optimization/StopCondition.js
+++ optimization/StopCondition.js
@@ -1,28 +1,39 @@
 
+/** Class representing the stop condition of the metaheuristics.
+ */
 class StopCondition {
 
-	constructor(){
-		this.maxSteps = 100;
-		this.maxNoUpdt = 10;
-		this.maxEvals = 10000;
+	/** Creates a stop condition with default values. 
+		@param {*} maxSteps - The maximum number of iterations (default 100).
+		@param {*} maxNoUpdt - The maximum number of iterations whitout update of the best solution (default 10).
+		@param {*} maxEvals - The maximum number of evaluations (default 10000).
+	 */
+	constructor(maxSteps = 100, maxNoUpdt = 10, maxEvals = 10000) {
+		this.maxSteps = maxSteps;
+		this.maxNoUpdt = maxNoUpdt;
+		this.maxEvals = maxEvals;
 		this.numSteps = 0;
 		this.lastUpdt = 0;
 		this.numEvals = 0;
 	}
 
-	nextStep(){
+	/**Increment the number of iterations by 1.*/
+	nextStep() {
 		this.numSteps += 1;
 	}
 
-	update(){
+	/**Indicates the best solution atualization.*/
+	update() {
 		this.lastUpdt = this.numSteps;
 	}
 
-	countEval(){
+	/**Increment the number of evaluations by 1.*/
+	countEval() {
 		this.numEvals += 1;
 	}
 
-	isRunning(){
+	/**Checks if any limit has been reached. */
+	isRunning() {
 		return (this.numSteps - this.lastUpdt) < this.maxNoUpdt
 			&& this.numSteps < this.maxSteps
 			&& this.numEvals < this.maxEvals;
